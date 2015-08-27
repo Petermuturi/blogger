0ad46b6a5dd3e53eca0f3a7d30fd053b15efe976
Peter Muturi <pmuturi65@gmail.com>
2015-08-27 10:08:57 +0300

    taggings

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,5 @@
 class Article < ActiveRecord::Base
 	has_many :comments
+	has_many :taggings
+
 end
---
+++ app/models/tagging.rb
@@ -0,0 +1,6 @@
+class Tagging < ActiveRecord::Base
+  belongs_to :tag
+  belongs_to :article
+  has_many :taggings
+
+end
