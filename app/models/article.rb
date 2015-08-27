08c607294e053097d0657b896cfb79dc23268aaa
Peter Muturi <pmuturi65@gmail.com>
2015-08-27 10:19:46 +0300

    changes

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,7 +2,8 @@ class Article < ActiveRecord::Base
 	has_many :comments
 	has_many :taggings
 has_many :tags, through: :taggings
-def tag_list
+
+def tag_list=(tags_string)
 	  self.tags.collect do |tag|
     tag.name
 
