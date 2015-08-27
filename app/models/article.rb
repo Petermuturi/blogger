32848694e9897a11a26e6e9c5fca8f934e672210
Peter Muturi <pmuturi65@gmail.com>
2015-08-27 10:18:13 +0300

    changes

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,12 @@
 class Article < ActiveRecord::Base
 	has_many :comments
 	has_many :taggings
+has_many :tags, through: :taggings
+def tag_list
+	  self.tags.collect do |tag|
+    tag.name
+
+  tags.join(", ")
+end
 
 end
--- app/models/tagging.rb
+++ app/models/tagging.rb
@@ -1,6 +1,4 @@
 class Tagging < ActiveRecord::Base
   belongs_to :tag
   belongs_to :article
-  has_many :taggings
-
-end
+ end
